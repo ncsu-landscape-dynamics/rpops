f554f09b917f2a29a472d2d36cda61b924ba1925
ChrisJones687 <cjones1688@gmail.com>
2018-09-19 06:30:32 -0400

    updated to latest version of pops library and set parameters for test in correct order.

--- pops.cpp
+++ pops.cpp
@@ -5,10 +5,11 @@ using namespace Rcpp;
 #include <iostream>
 
 using std::cout;
+using namespace pops;
 
 // [[Rcpp::plugins(cpp11)]]
 // [[Rcpp::export]]
-NumericVector pops(NumericVector x) {
+NumericVector pops_model(NumericVector x) {
   Raster<int> infected = {{5, 0}, {0, 0}};
   Raster<int> mortality_tracker = {{0, 0}, {0, 0}};
   Raster<int> susceptible = {{10, 6}, {14, 15}};
@@ -16,7 +17,7 @@ NumericVector pops(NumericVector x) {
   Raster<double> temperature = {{5, 0}, {0, 0}};
   Raster<double> weather_coefficient = {{0.8, 0.8}, {0.2, 0.8}};
   std::vector<std::tuple<int, int>> outside_dispersers;
-  Dispersal_kernel dispersal_kernel = CAUCHY;
+  DispersalKernel dispersal_kernel = CAUCHY;
   bool weather = true;
   double lethal_temperature = -4.5;
   double reproductive_rate = 4.5;
@@ -27,7 +28,7 @@ NumericVector pops(NumericVector x) {
   simulation.generate(infected, weather, weather_coefficient, reproductive_rate);
   simulation.disperse(susceptible, infected,
                       mortality_tracker, total_plants,
-                      outside_dispersers, weather_coefficient, weather,
+                      outside_dispersers, weather, weather_coefficient, 
                       dispersal_kernel, short_distance_scale);
   cout << infected;
   cout << outside_dispersers.size() << endl;
