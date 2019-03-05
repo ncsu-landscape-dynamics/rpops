cc8cf3a22b6598e695cbdc7a10f90e955070823d
ChrisJones687 <cjones1688@gmail.com>
2019-03-05 08:29:54 -0500

    added spacing to match hadley styling for R packages

--- R/quantity_allocation_disagreement.R
+++ R/quantity_allocation_disagreement.R
@@ -32,18 +32,18 @@ quantity_allocation_disagreement <- function(reference, comparison){
   #calculate number of infected patches
   reference_no0 <- reference
   comparison_no0 <- comparison
-  reference_no0[reference_no0==0]<-NA
-  comparison_no0[comparison_no0==0]<-NA
-  NP_ref_full <- landscapemetrics::lsm_c_np(reference_no0, directions=8)
-  NP_comp_full <- landscapemetrics::lsm_c_np(comparison_no0, directions=8)
+  reference_no0[reference_no0 == 0] <- NA
+  comparison_no0[comparison_no0 == 0] <- NA
+  NP_ref_full <- landscapemetrics::lsm_c_np(reference_no0, directions = 8)
+  NP_comp_full <- landscapemetrics::lsm_c_np(comparison_no0, directions = 8)
   NP_ref <- NP_ref_full$value
   NP_comp <- NP_comp_full$value
   change_NP <- abs((NP_comp - NP_ref)/NP_ref)
   
   #calculate the mean euclidean distance between patches
   if (NP_comp > 1) {
-    ENN_MN_ref_full <- landscapemetrics::lsm_c_enn_mn(reference_no0, directions=8, verbose=TRUE)
-    ENN_MN_comp_full <- landscapemetrics::lsm_c_enn_mn(comparison_no0, directions=8, verbose=TRUE)
+    ENN_MN_ref_full <- landscapemetrics::lsm_c_enn_mn(reference_no0, directions = 8, verbose = TRUE)
+    ENN_MN_comp_full <- landscapemetrics::lsm_c_enn_mn(comparison_no0, directions = 8, verbose = TRUE)
     ENN_MN_ref <- ENN_MN_ref_full$value
     ENN_MN_comp <- ENN_MN_comp_full$value
     change_ENN_MN <- abs((ENN_MN_comp - ENN_MN_ref)/ENN_MN_ref)
@@ -53,8 +53,8 @@ quantity_allocation_disagreement <- function(reference, comparison){
   }
   
   # calculate the mean perimeter-area ratio of patches and the difference
-  PARA_MN_ref_full <- landscapemetrics::lsm_c_para_mn(reference_no0, directions=8)
-  PARA_MN_comp_full <- landscapemetrics::lsm_c_para_mn(comparison_no0, directions=8)
+  PARA_MN_ref_full <- landscapemetrics::lsm_c_para_mn(reference_no0, directions = 8)
+  PARA_MN_comp_full <- landscapemetrics::lsm_c_para_mn(comparison_no0, directions = 8)
   PARA_MN_ref <- PARA_MN_ref_full$value
   PARA_MN_comp <- PARA_MN_comp_full$value
   change_PARA_MN <- abs((PARA_MN_comp - PARA_MN_ref)/PARA_MN_ref) 
@@ -70,13 +70,13 @@ quantity_allocation_disagreement <- function(reference, comparison){
   LSI <- 1 - ((change_NP + change_ENN_MN + change_PARA_MN + change_LPI)/8)
   
   ## create data frame for comparison
-  output <- data.frame(quantity_disagreement = 0, allocation_disagreement = 0, total_disagreement = 0 , omission = 0, commission = 0 ,number_of_infected_comp =0,directional_disagreement = 0, landscape_similarity=0)
+  output <- data.frame(quantity_disagreement = 0, allocation_disagreement = 0, total_disagreement = 0 , omission = 0, commission = 0 , number_of_infected_comp =0, directional_disagreement = 0, landscape_similarity=0)
   output$total_disagreement <- sum(abs(compare2))
-  output$quantity_disagreement <- abs(sum(reference2)-sum(comparison2))
+  output$quantity_disagreement <- abs(sum(reference2) - sum(comparison2))
   output$allocation_disagreement <- output$total_disagreement - output$quantity_disagreement
-  output$omission <- abs(sum(compare2[compare2== 1]))
-  output$commission <- abs(sum(compare2[compare2== -1]))
-  output$number_of_infected_comp <- sum(comparison[comparison==1])
+  output$omission <- abs(sum(compare2[compare2 == 1]))
+  output$commission <- abs(sum(compare2[compare2 == -1]))
+  output$number_of_infected_comp <- sum(comparison[comparison ==1])
   output$directional_disagreement <- sum(compare2)
   output$landscape_similarity <- LSI
   
