14136eeeebb97d729e89a649ee8c79c19e2231b8
Chris Michael Jones <cjones1688@gmail.com>
2019-03-08 10:05:41 -0500

    Updated the calibratate function to use quantity, allocation, and landscape similarity for MCMC parameter estimation.

--- R/calibrate.R
+++ R/calibrate.R
@@ -335,7 +335,7 @@ calibrate <- function(infected_years_file, num_interations, start_reproductive_r
   
   ## Create function for MCMC runs
   MCMC <- function(num_iterations, start_reproductive_rate, start_short_distance_scale, sd_reproductive_rate, sd_short_distance_scale){
-    params <- data.frame(reproductive_rate = rep(0, num_iterations + 1), short_distance_scale = rep(0, num_iterations + 1), total_disagreement = rep(0, num_iterations + 1), number_of_infected_difference = rep(0, num_iterations + 1), directional_disagreement = rep(0, num_iterations + 1))
+    params <- data.frame(reproductive_rate = rep(0, num_iterations + 1), short_distance_scale = rep(0, num_iterations + 1), total_disagreement = rep(0, num_iterations + 1), quantity_disagreement = rep(0, num_iterations + 1), allocation_disagreement = rep(0, num_iterations + 1), landscape_similarity = rep(0, num_iterations + 1), number_of_infected_difference = rep(0, num_iterations + 1), directional_disagreement = rep(0, num_iterations + 1))
     params$reproductive_rate[1] <- start_reproductive_rate
     params$short_distance_scale[1] <- start_short_distance_scale
     data <- param_func(start_reproductive_rate, start_short_distance_scale)
@@ -373,14 +373,14 @@ calibrate <- function(infected_years_file, num_interations, start_reproductive_r
     current_short_distance_scale <- start_short_distance_scale
     proposed_short_distance_scale <- 0.0
     while (proposed_short_distance_scale <= 0.0) {
-      proposed_short_distance_scale <- round(abs(rnorm(1, mean=current_short_distance_scale, sd=sd_short_distance_scale)), digits = 0)
+      proposed_short_distance_scale <- round(abs(rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
     }
     
     params$reproductive_rate[2] <- proposed_reproductive_rate
     params$short_distance_scale <- proposed_short_distance_scale
-    i = 2
+    i <- 2
+    
     while(i <= num_iterations){
-
       
       data <- param_func(proposed_reproductive_rate, proposed_short_distance_scale)
       
@@ -401,48 +401,89 @@ calibrate <- function(infected_years_file, num_interations, start_reproductive_r
       
       total_disagreement <- 0
       directional_disagreement <- 0
-      all_disagreement <- data.frame(quantity_disagreement = 0, allocation_disagreement = 0, total_disagreement = 0 , omission = 0, commission = 0 ,number_of_infected_comp = 0, directional_disagreement = 0)
+      landscape_similarity <- 0
+      all_disagreement <- data.frame(quantity_disagreement = 0, allocation_disagreement = 0, total_disagreement = 0 , omission = 0, commission = 0 ,number_of_infected_comp = 0, directional_disagreement = 0, landscape_similarity = 0)
       for (p in 1:min(raster::nlayers(comp_years), raster::nlayers(infection_years))) {
         all_disagreement[p,] <- quantity_allocation_disagreement(infection_years[[p]], comp_years[[p]])
         total_disagreement[p] <- all_disagreement$total_disagreement[p]
         directional_disagreement[p] <- all_disagreement$directional_disagreement[p]
+        landscape_similarity[p] <- all_disagreement$landscape_similarity[p]
       }
       
       params$total_disagreement[i] <- sum(total_disagreement)
       params$directional_disagreement[i] <- sum(directional_disagreement)
+      params$landscape_similarity[i] <- mean(landscape_similarity)
+      params$quantity_disagreement[i] <- sum(all_disagreement$quantity_disagreement)
+      params$allocation_disagreement[i] <- sum(all_disagreement$allocation_disagreement)
       params$number_of_infected_difference[i] <- sum(abs(total_infections - comp_total_infections))
       
       accept <- FALSE
       
-      if(params$total_disagreement[i] <= params$total_disagreement[i-1]){ # accept change if model improves or doesn't change
+      if ((params$allocation_disagreement[i] <= params$allocation_disagreement[i-1]) && (params$landscape_similarity[i] >= params$landscape_similarity[i-1])) { # accept change if model improves or doesn't change
         current_short_distance_scale <- proposed_short_distance_scale
         proposed_short_distance_scale <- 0
         while (proposed_short_distance_scale <= 0) {
-          if (management == TRUE) {
-            proposed_short_distance_scale <- round(abs(rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
-          } else if (management == FALSE) {
-            if (params$directional_disagreement[i] <= 0) {
-              proposed_short_distance_scale <- round(current_short_distance_scale - abs(current_short_distance_scale - rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
-            } else if (params$directional_disagreement[i] > 0) {
-              proposed_short_distance_scale <- round(current_short_distance_scale + abs(current_short_distance_scale - rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
-            }
-          }
+          proposed_short_distance_scale <- round(abs(rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
+          # if (management == TRUE) {
+          #   proposed_short_distance_scale <- round(abs(rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
+          # } else if (management == FALSE) {
+          #   if (params$directional_disagreement[i] <= 0) {
+          #     proposed_short_distance_scale <- round(current_short_distance_scale - abs(current_short_distance_scale - rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
+          #   } else if (params$directional_disagreement[i] > 0) {
+          #     proposed_short_distance_scale <- round(current_short_distance_scale + abs(current_short_distance_scale - rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
+          #   }
+          # }
         }
         accept <- TRUE
-      } else if ((1 - ((params$total_disagreement[i] - params$total_disagreement[i-1])/(params$total_disagreement[i] + params$total_disagreement[i-1]))) < runif(1)) {
+      } else if (((params$allocation_disagreement[i] <= params$allocation_disagreement[i-1]) && ((1 - (abs(params$landscape_similarity[i] - params$landscape_similarity[i-1])/abs(params$landscape_similarity[i] + params$landscape_similarity[i-1]))) <= runif(1)))) {
         # accept change randomly if model is worse than previous run
         current_short_distance_scale <- proposed_short_distance_scale
         proposed_short_distance_scale <- 0
         while (proposed_short_distance_scale <= 0) {
-          if (management == TRUE) {
-            proposed_short_distance_scale <- round(abs(rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
-          } else if (management == FALSE) {
-            if (params$directional_disagreement[i] <= 0) {
-              proposed_short_distance_scale <- round(current_short_distance_scale - abs(current_short_distance_scale - rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
-            } else if (params$directional_disagreement[i] > 0) {
-              proposed_short_distance_scale <- round(current_short_distance_scale + abs(current_short_distance_scale - rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
-            }
-          }
+          proposed_short_distance_scale <- round(abs(rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
+          # if (management == TRUE) {
+          #   proposed_short_distance_scale <- round(abs(rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
+          # } else if (management == FALSE) {
+          #   if (params$directional_disagreement[i] <= 0) {
+          #     proposed_short_distance_scale <- round(current_short_distance_scale - abs(current_short_distance_scale - rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
+          #   } else if (params$directional_disagreement[i] > 0) {
+          #     proposed_short_distance_scale <- round(current_short_distance_scale + abs(current_short_distance_scale - rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
+          #   }
+          # }
+        }
+        accept <- TRUE
+      } else if ((params$landscape_similarity[i] >= params$landscape_similarity[i-1]) && ((1 - (abs(params$allocation_disagreement[i] - params$allocation_disagreement[i-1])/abs(params$allocation_disagreement[i] + params$allocation_disagreement[i-1]))) <= runif(1))) {
+        # accept change randomly if model is worse than previous run
+        current_short_distance_scale <- proposed_short_distance_scale
+        proposed_short_distance_scale <- 0
+        while (proposed_short_distance_scale <= 0) {
+          proposed_short_distance_scale <- round(abs(rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
+          # if (management == TRUE) {
+          #   proposed_short_distance_scale <- round(abs(rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
+          # } else if (management == FALSE) {
+          #   if (params$directional_disagreement[i] <= 0) {
+          #     proposed_short_distance_scale <- round(current_short_distance_scale - abs(current_short_distance_scale - rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
+          #   } else if (params$directional_disagreement[i] > 0) {
+          #     proposed_short_distance_scale <- round(current_short_distance_scale + abs(current_short_distance_scale - rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
+          #   }
+          # }
+        }
+        accept <- TRUE
+      } else if (((params$allocation_disagreement[i] > params$allocation_disagreement[i-1]) && (params$landscape_similarity[i] < params$landscape_similarity[i-1])) && (((1 - (abs(params$landscape_similarity[i] - params$landscape_similarity[i-1])/abs(params$landscape_similarity[i] + params$landscape_similarity[i-1]))) <= runif(1)) && ((1 - (abs(params$allocation_disagreement[i] - params$allocation_disagreement[i-1])/abs(params$allocation_disagreement[i] + params$allocation_disagreement[i-1]))) <= runif(1)))) {
+        # accept change randomly if model is worse than previous run
+        current_short_distance_scale <- proposed_short_distance_scale
+        proposed_short_distance_scale <- 0
+        while (proposed_short_distance_scale <= 0) {
+          proposed_short_distance_scale <- round(abs(rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
+          # if (management == TRUE) {
+          #   proposed_short_distance_scale <- round(abs(rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
+          # } else if (management == FALSE) {
+          #   if (params$directional_disagreement[i] <= 0) {
+          #     proposed_short_distance_scale <- round(current_short_distance_scale - abs(current_short_distance_scale - rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
+          #   } else if (params$directional_disagreement[i] > 0) {
+          #     proposed_short_distance_scale <- round(current_short_distance_scale + abs(current_short_distance_scale - rnorm(1, mean = current_short_distance_scale, sd = sd_short_distance_scale)), digits = 0)
+          #   }
+          # }
         }
         accept <- TRUE
       } else {
@@ -450,36 +491,73 @@ calibrate <- function(infected_years_file, num_interations, start_reproductive_r
         proposed_short_distance_scale <- current_short_distance_scale
       }
       
-      if(params$number_of_infected_difference[i] <= params$number_of_infected_difference[i-1]){
+      if ((params$number_of_infected_difference[i] <= params$number_of_infected_difference[i-1]) && (params$quantity_disagreement[i] <= params$quantity_disagreement[i-1])) {
         # accept change if model improves or doesn't change
         current_reproductive_rate <- proposed_reproductive_rate
         proposed_reproductive_rate <-  0
         while (proposed_reproductive_rate <= 0) {
-          if (management == TRUE) {
-            proposed_reproductive_rate <- round(rnorm(1,mean = current_reproductive_rate,sd = sd_reproductive_rate), digits = 1)
-          } else if (management == FALSE) {
-            if (sum(total_infections - comp_total_infections) <= 0) {
-              proposed_reproductive_rate = round(current_reproductive_rate - abs(current_reproductive_rate - rnorm(1, mean = current_reproductive_rate, sd = sd_reproductive_rate)), digits = 1)
-            } else if (sum(total_infections - comp_total_infections) > 0) {
-              proposed_reproductive_rate = round(current_reproductive_rate + abs(current_reproductive_rate - rnorm(1, mean = current_reproductive_rate, sd = sd_reproductive_rate)), digits = 1)
-            }
-          }
+          proposed_reproductive_rate <- round(rnorm(1,mean = current_reproductive_rate,sd = sd_reproductive_rate), digits = 1)
+          # if (management == TRUE) {
+          #   proposed_reproductive_rate <- round(rnorm(1,mean = current_reproductive_rate,sd = sd_reproductive_rate), digits = 1)
+          # } else if (management == FALSE) {
+          #   if (sum(total_infections - comp_total_infections) <= 0) {
+          #     proposed_reproductive_rate = round(current_reproductive_rate - abs(current_reproductive_rate - rnorm(1, mean = current_reproductive_rate, sd = sd_reproductive_rate)), digits = 1)
+          #   } else if (sum(total_infections - comp_total_infections) > 0) {
+          #     proposed_reproductive_rate = round(current_reproductive_rate + abs(current_reproductive_rate - rnorm(1, mean = current_reproductive_rate, sd = sd_reproductive_rate)), digits = 1)
+          #   }
+          # }
         }
         accept <- TRUE
-      } else if ((1 - ((params$number_of_infected_difference[i] - params$number_of_infected_difference[i-1])/(params$number_of_infected_difference[i] + params$number_of_infected_difference[i-1]))) < runif(1)) {
+      } else if (((params$number_of_infected_difference[i] <= params$number_of_infected_difference[i-1]) && ((1 - (abs(params$quantity_disagreement[i] - params$quantity_disagreement[i-1])/(params$quantity_disagreement[i] + params$quantity_disagreement[i-1]))) <= runif(1)))) {
         # accept change randomly if model is worse than previous run
         current_reproductive_rate <- proposed_reproductive_rate
         proposed_reproductive_rate <-  0
         while (proposed_reproductive_rate <= 0) {
-          if (management == TRUE) {
-            proposed_reproductive_rate <- round(rnorm(1,mean = current_reproductive_rate,sd = sd_reproductive_rate), digits = 1)
-          } else if (management == FALSE) {
-            if (sum(total_infections - comp_total_infections) <= 0) {
-              proposed_reproductive_rate <- round(current_reproductive_rate - abs(current_reproductive_rate - rnorm(1, mean = current_reproductive_rate, sd = sd_reproductive_rate)), digits = 1)
-            } else if (sum(total_infections - comp_total_infections) > 0) {
-              proposed_reproductive_rate <- round(current_reproductive_rate + abs(current_reproductive_rate - rnorm(1, mean = current_reproductive_rate, sd = sd_reproductive_rate)), digits = 1)
-            }
-          }
+          proposed_reproductive_rate <- round(rnorm(1,mean = current_reproductive_rate,sd = sd_reproductive_rate), digits = 1)
+          # if (management == TRUE) {
+          #   proposed_reproductive_rate <- round(rnorm(1,mean = current_reproductive_rate,sd = sd_reproductive_rate), digits = 1)
+          # } else if (management == FALSE) {
+          #   if (sum(total_infections - comp_total_infections) <= 0) {
+          #     proposed_reproductive_rate <- round(current_reproductive_rate - abs(current_reproductive_rate - rnorm(1, mean = current_reproductive_rate, sd = sd_reproductive_rate)), digits = 1)
+          #   } else if (sum(total_infections - comp_total_infections) > 0) {
+          #     proposed_reproductive_rate <- round(current_reproductive_rate + abs(current_reproductive_rate - rnorm(1, mean = current_reproductive_rate, sd = sd_reproductive_rate)), digits = 1)
+          #   }
+          # }
+        }
+        accept <- TRUE
+      } else if (((params$quantity_disagreement[i] <= params$quantity_disagreement[i-1]) && ((1 - (abs(params$number_of_infected_difference[i] - params$number_of_infected_difference[i-1])/(params$number_of_infected_difference[i] + params$number_of_infected_difference[i-1]))) <= runif(1)))) {
+        # accept change randomly if model is worse than previous run
+        current_reproductive_rate <- proposed_reproductive_rate
+        proposed_reproductive_rate <-  0
+        while (proposed_reproductive_rate <= 0) {
+          proposed_reproductive_rate <- round(rnorm(1,mean = current_reproductive_rate,sd = sd_reproductive_rate), digits = 1)
+          # if (management == TRUE) {
+          #   proposed_reproductive_rate <- round(rnorm(1,mean = current_reproductive_rate,sd = sd_reproductive_rate), digits = 1)
+          # } else if (management == FALSE) {
+          #   if (sum(total_infections - comp_total_infections) <= 0) {
+          #     proposed_reproductive_rate <- round(current_reproductive_rate - abs(current_reproductive_rate - rnorm(1, mean = current_reproductive_rate, sd = sd_reproductive_rate)), digits = 1)
+          #   } else if (sum(total_infections - comp_total_infections) > 0) {
+          #     proposed_reproductive_rate <- round(current_reproductive_rate + abs(current_reproductive_rate - rnorm(1, mean = current_reproductive_rate, sd = sd_reproductive_rate)), digits = 1)
+          #   }
+          # }
+        }
+        accept <- TRUE
+      }
+      else if (((params$number_of_infected_difference[i] > params$number_of_infected_difference[i-1]) && (params$quantity_disagreement[i] > params$quantity_disagreement[i-1])) && (((1 - (abs(params$number_of_infected_difference[i] - params$number_of_infected_difference[i-1])/(params$number_of_infected_difference[i] + params$number_of_infected_difference[i-1]))) <= runif(1)) && ((1 - (abs(params$quantity_disagreement[i] - params$quantity_disagreement[i-1])/(params$quantity_disagreement[i] + params$quantity_disagreement[i-1]))) <= runif(1)))) {
+        # accept change randomly if model is worse than previous run
+        current_reproductive_rate <- proposed_reproductive_rate
+        proposed_reproductive_rate <-  0
+        while (proposed_reproductive_rate <= 0) {
+          proposed_reproductive_rate <- round(rnorm(1,mean = current_reproductive_rate,sd = sd_reproductive_rate), digits = 1)
+          # if (management == TRUE) {
+          #   proposed_reproductive_rate <- round(rnorm(1,mean = current_reproductive_rate,sd = sd_reproductive_rate), digits = 1)
+          # } else if (management == FALSE) {
+          #   if (sum(total_infections - comp_total_infections) <= 0) {
+          #     proposed_reproductive_rate <- round(current_reproductive_rate - abs(current_reproductive_rate - rnorm(1, mean = current_reproductive_rate, sd = sd_reproductive_rate)), digits = 1)
+          #   } else if (sum(total_infections - comp_total_infections) > 0) {
+          #     proposed_reproductive_rate <- round(current_reproductive_rate + abs(current_reproductive_rate - rnorm(1, mean = current_reproductive_rate, sd = sd_reproductive_rate)), digits = 1)
+          #   }
+          # }
         }
         accept <- TRUE
       } else {
--- R/quantity_allocation_disagreement.R
+++ R/quantity_allocation_disagreement.R
@@ -22,63 +22,88 @@
 #' quantity_allocation_disagreement(reference, comparison)
 #' 
 quantity_allocation_disagreement <- function(reference, comparison){
-  ## test that the comparison raster is the same extent, resolution, and crs as the reference (if not end)
+  # test that the comparison raster is the same extent, resolution, and crs as the reference (if not end)
   raster::compareRaster(reference, comparison)
   compare <- reference - comparison
-  compare2 <- raster::as.matrix(compare)
-  reference2 <- raster::as.matrix(reference)
-  comparison2 <- raster::as.matrix(comparison)
+  # compare3 <- reference + comparison
+  # extent = extent(reference)
+  # num_of_cells = max(cellsFromExtent(reference, extent(reference)))
   
-  #calculate number of infected patches
+  # calculate number of infected patches
   reference_no0 <- reference
   comparison_no0 <- comparison
   reference_no0[reference_no0 == 0] <- NA
   comparison_no0[comparison_no0 == 0] <- NA
-  NP_ref_full <- landscapemetrics::lsm_c_np(reference_no0, directions = 8)
-  NP_comp_full <- landscapemetrics::lsm_c_np(comparison_no0, directions = 8)
-  NP_ref <- NP_ref_full$value
-  NP_comp <- NP_comp_full$value
-  change_NP <- abs((NP_comp - NP_ref)/NP_ref)
-  
-  #calculate the mean euclidean distance between patches
-  if (NP_comp > 1) {
-    ENN_MN_ref_full <- landscapemetrics::lsm_c_enn_mn(reference_no0, directions = 8, verbose = TRUE)
-    ENN_MN_comp_full <- landscapemetrics::lsm_c_enn_mn(comparison_no0, directions = 8, verbose = TRUE)
-    ENN_MN_ref <- ENN_MN_ref_full$value
-    ENN_MN_comp <- ENN_MN_comp_full$value
-    change_ENN_MN <- abs((ENN_MN_comp - ENN_MN_ref)/ENN_MN_ref)
-  } else  if (NP_comp == 1) {
-    ENN_MN_comp <- NA
-    change_ENN_MN <- NA
+  NP_ref <- landscapemetrics::lsm_c_np(reference_no0, directions = 8)$value
+  if (sum(comparison_no0[comparison_no0 >0]) == 0) {
+    NP_comp <- 0
+    ENN_MN_comp <- 0
+    PARA_MN_comp <- 0
+    LPI_comp <- 0
+  } else {
+    NP_comp <- landscapemetrics::lsm_c_np(comparison_no0, directions = 8)$value
   }
   
+  change_NP <- abs((NP_comp - NP_ref)/NP_ref)
+  
+  # calculate the mean euclidean distance between patches
+  if (NP_ref > 1) {
+    ENN_MN_ref <- landscapemetrics::lsm_c_enn_mn(reference_no0, directions = 8, verbose = TRUE)$value
+  } else  if (NP_ref == 1) {
+    ENN_MN_ref <- 0
+  }
+  
+  if (sum(comparison_no0[comparison_no0 > 0]) != 0 && NP_comp > 1) {
+    ENN_MN_comp <- landscapemetrics::lsm_c_enn_mn(comparison_no0, directions = 8, verbose = TRUE)$value
+  } else  if (sum(comparison_no0[comparison_no0 > 0]) != 0 && NP_comp <= 1) {
+    ENN_MN_comp <- 0
+  }
+  
+  if (ENN_MN_ref != 0) {
+    change_ENN_MN <- abs((ENN_MN_comp - ENN_MN_ref)/ENN_MN_ref)
+  } else if (ENN_MN_comp == 0 && ENN_MN_ref == 0) {
+    change_ENN_MN <- 0
+  } else {
+    change_ENN_MN <- 1
+  }
+
   # calculate the mean perimeter-area ratio of patches and the difference
-  PARA_MN_ref_full <- landscapemetrics::lsm_c_para_mn(reference_no0, directions = 8)
-  PARA_MN_comp_full <- landscapemetrics::lsm_c_para_mn(comparison_no0, directions = 8)
-  PARA_MN_ref <- PARA_MN_ref_full$value
-  PARA_MN_comp <- PARA_MN_comp_full$value
+  PARA_MN_ref <- landscapemetrics::lsm_c_para_mn(reference_no0, directions = 8)$value
+  if (sum(comparison_no0[comparison_no0 >0]) == 0) {
+    PARA_MN_comp <- 0
+  } else if (sum(comparison_no0[comparison_no0 >0]) != 0) {
+    PARA_MN_comp <- landscapemetrics::lsm_c_para_mn(comparison_no0, directions = 8)$value
+  }
+    
   change_PARA_MN <- abs((PARA_MN_comp - PARA_MN_ref)/PARA_MN_ref) 
   
   # calculate the largest patch index and difference
-  LPI_ref_full <- landscapemetrics::lsm_c_lpi(reference_no0, directions=8)
-  LPI_comp_full <- landscapemetrics::lsm_c_lpi(comparison_no0, directions=8)
-  LPI_ref <- LPI_ref_full$value
-  LPI_comp <- LPI_comp_full$value
+  LPI_ref <- landscapemetrics::lsm_c_lpi(reference_no0, directions = 8)$value
+  if (sum(comparison_no0[comparison_no0 >0]) == 0) {
+    LPI_comp <- 0
+  } else if (sum(comparison_no0[comparison_no0 >0]) != 0) {
+    LPI_comp <- landscapemetrics::lsm_c_lpi(comparison_no0, directions = 8)$value
+  }
+  
   change_LPI <- abs(LPI_comp - LPI_ref) / 100
   
   # calculate landscape similarity index between reference and comparison
-  LSI <- 1 - ((change_NP + change_ENN_MN + change_PARA_MN + change_LPI)/8)
+  LSI <- 1 - ((change_NP + change_ENN_MN + change_PARA_MN + change_LPI) / 4)
+  if (LSI < 0) { LSI <- 0 }
   
   ## create data frame for comparison
-  output <- data.frame(quantity_disagreement = 0, allocation_disagreement = 0, total_disagreement = 0 , omission = 0, commission = 0 , number_of_infected_comp =0, directional_disagreement = 0, landscape_similarity = 0)
-  output$total_disagreement <- sum(abs(compare2))
-  output$quantity_disagreement <- abs(sum(reference2) - sum(comparison2))
+  output <- data.frame(quantity_disagreement = 0, allocation_disagreement = 0, total_disagreement = 0, omission = 0, commission = 0 , number_of_infected_comp = 0, directional_disagreement = 0, landscape_similarity = 0)
+  output$total_disagreement <- sum(compare[compare == 1]) + abs(sum(compare[compare == -1]))
+  output$quantity_disagreement <- abs(sum(compare[compare == 1]) + sum(compare[compare == -1]))
   output$allocation_disagreement <- output$total_disagreement - output$quantity_disagreement
-  output$omission <- abs(sum(compare2[compare2 == 1]))
-  output$commission <- abs(sum(compare2[compare2 == -1]))
-  output$number_of_infected_comp <- sum(comparison[comparison ==1])
-  output$directional_disagreement <- sum(compare2)
+  output$omission <- abs(sum(compare[compare == 1]))
+  output$commission <- abs(sum(compare[compare == -1]))
+  output$number_of_infected_comp <- sum(comparison[comparison == 1])
+  output$directional_disagreement <- sum(compare[compare == 1]) + sum(compare[compare == -1])
   output$landscape_similarity <- LSI
+  # output$true_positives <- abs(sum(compare3[compare3 ==2]))
+  # output$true_negatives <- num_of_cells - output$omission - output$commission - output$true_positives
+  # output$odds_ratio = (output$true_positives*output$true_negatives)/(output$omission*output$commission)
   
   return(output)
 }
