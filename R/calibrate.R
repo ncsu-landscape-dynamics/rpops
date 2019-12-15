d69897cf0544cfb4df9398fc87c9250618f7f8fb
ChrisJones687 <cjones1688@gmail.com>
2019-12-15 07:14:27 -0500

    updated the new calibrate function to work with the new start and end dates

--- R/calibrate.R
+++ R/calibrate.R
@@ -23,6 +23,7 @@
 #' @importFrom foreach  registerDoSEQ %dopar% %do% %:% foreach
 #' @importFrom parallel makeCluster stopCluster detectCores
 #' @importFrom iterators icount
+#' @importFrom lubridate interval time_length
 #' 
 #' @return a dataframe of the variables saved and their success metrics for each run
 #' 
@@ -39,7 +40,7 @@ calibrate <- function(infected_years_file, num_iterations, start_reproductive_ra
                       precip = FALSE, precipitation_coefficient_file = "", 
                       time_step = "month", reproductive_rate = 3.0,
                       season_month_start = 1, season_month_end = 12, 
-                      start_time = 2018, end_time = 2020, 
+                      start_date = '2008-01-01', end_date = '2008-12-31', 
                       use_lethal_temperature = FALSE, temperature_file = "",
                       lethal_temperature = -12.87, lethal_temperature_month = 1,
                       mortality_on = FALSE, mortality_rate = 0, mortality_time_lag = 0, 
@@ -97,19 +98,17 @@ calibrate <- function(infected_years_file, num_iterations, start_reproductive_ra
     return("Time step must be one of 'week', 'month' or 'day'")
   }
   
-  if (class(end_time) != "numeric" || nchar(end_time) != 4 || class(start_time) != "numeric" || nchar(start_time) != 4){
-    return("End time and/or start time not of type numeric and/or in format YYYY")
-  }
+  duration <- lubridate::interval(start_date, end_date)
   
   if (time_step == "week") {
-    number_of_time_steps <- (end_time-start_time+1)*52
+    number_of_time_steps <- ceiling(time_length(duration, "week"))
   } else if (time_step == "month") {
-    number_of_time_steps <- (end_time-start_time+1)*12
+    number_of_time_steps <- ceiling(time_length(duration, "month"))
   } else if (time_step == "day") {
-    number_of_time_steps <- (end_time-start_time+1)*365
+    number_of_time_steps <- ceiling(time_length(duration, "day"))
   }
   
-  number_of_years <- end_time-start_time+1
+  number_of_years <- ceiling(time_length(duration, "year"))
   
   infected <- raster::raster(infected_file)
   infected <- raster::reclassify(infected, matrix(c(NA,0), ncol = 2, byrow = TRUE), right = NA)
@@ -364,7 +363,7 @@ calibrate <- function(infected_years_file, num_iterations, start_reproductive_ra
                        time_step = time_step, reproductive_rate = reproductive_rate,
                        mortality_rate = mortality_rate, mortality_time_lag = mortality_time_lag,
                        season_month_start = season_month_start, season_month_end = season_month_end,
-                       start_time = start_time, end_time = end_time,
+                       start_date = start_date, end_date = end_date,
                        treatment_method = treatment_method,
                        natural_kernel_type = natural_kernel_type, anthropogenic_kernel_type = anthropogenic_kernel_type, 
                        use_anthropogenic_kernel = use_anthropogenic_kernel, percent_natural_dispersal = percent_natural_dispersal,
