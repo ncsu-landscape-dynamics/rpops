5c0013059ca59ff6559c94dedd614b59573d2963
ChrisJones687 <cjones1688@gmail.com>
2020-09-13 11:04:06 -0400

    formatted checks with styler

--- R/checks.R
+++ R/checks.R
@@ -11,7 +11,7 @@ initial_raster_checks <- function(x) {
   }
 
   if (checks_passed && !all((raster::extension(x) %in%
-                             c(".grd", ".tif", ".img")))) {
+    c(".grd", ".tif", ".img")))) {
     checks_passed <- FALSE
     failed_check <- "file is not one of '.grd', '.tif', '.img'"
   }
@@ -19,7 +19,8 @@ initial_raster_checks <- function(x) {
   if (checks_passed) {
     r <- raster::stack(x)
     r <- raster::reclassify(r, matrix(c(NA, 0), ncol = 2, byrow = TRUE),
-                            right = NA)
+      right = NA
+    )
   }
 
 
@@ -45,7 +46,7 @@ secondary_raster_checks <- function(x, x2) {
   }
 
   if (checks_passed && !all((raster::extension(x) %in%
-                             c(".grd", ".tif", ".img")))) {
+    c(".grd", ".tif", ".img")))) {
     checks_passed <- FALSE
     failed_check <- "file is not one of '.grd', '.tif', '.img'"
   }
@@ -53,7 +54,8 @@ secondary_raster_checks <- function(x, x2) {
   if (checks_passed) {
     r <- raster::stack(x)
     r2 <- raster::reclassify(r, matrix(c(NA, 0), ncol = 2, byrow = TRUE),
-                             right = NA)
+      right = NA
+    )
     if (!(raster::extent(r2) == raster::extent(r))) {
       raster::extent(r2) <- raster::extent(r)
     }
@@ -63,22 +65,22 @@ secondary_raster_checks <- function(x, x2) {
   if (checks_passed && !(raster::extent(x2) == raster::extent(r))) {
     checks_passed <- FALSE
     failed_check <-
-    "Extents of input rasters do not match. Ensure that all of your input
+      "Extents of input rasters do not match. Ensure that all of your input
     rasters have the same extent"
   }
 
   if (checks_passed && !(raster::xres(x2) == raster::xres(r) &&
-                         raster::yres(x2) == raster::yres(r))) {
+    raster::yres(x2) == raster::yres(r))) {
     checks_passed <- FALSE
     failed_check <-
-    "Resolution of input rasters do not match. Ensure that all of your input
+      "Resolution of input rasters do not match. Ensure that all of your input
     rasters have the same resolution"
   }
 
   if (checks_passed && !raster::compareCRS(r, x2)) {
     checks_passed <- FALSE
     failed_check <-
-    "Coordinate reference system (crs) of input rasters do not match. Ensure
+      "Coordinate reference system (crs) of input rasters do not match. Ensure
     that all of your input rasters have the same crs"
   }
 
@@ -103,13 +105,13 @@ treatment_checks <- function(treatment_stack,
   if (checks_passed && length(treatments_file) != length(treatment_dates)) {
     checks_passed <- FALSE
     failed_check <-
-    "Length of list for treatment dates and treatments_file must be equal"
+      "Length of list for treatment dates and treatments_file must be equal"
   }
 
   if (checks_passed && length(pesticide_duration) != length(treatment_dates)) {
     checks_passed <- FALSE
     failed_check <-
-    "Length of list for treatment dates and pesticide_duration must be equal"
+      "Length of list for treatment dates and pesticide_duration must be equal"
   }
 
   if (checks_passed) {
@@ -127,7 +129,6 @@ treatment_checks <- function(treatment_stack,
             raster::as.matrix(treatment_stack[[i]] * pesticide_efficacy)
         } else {
           treatment_maps[[i]] <- raster::as.matrix(treatment_stack[[i]])
-
         }
       }
     }
@@ -178,7 +179,7 @@ metric_checks <- function(success_metric) {
   } else {
     checks_passed <- FALSE
     failed_check <-
-    "Success metric must be one of 'quantity','quantity and configuration',
+      "Success metric must be one of 'quantity','quantity and configuration',
     'residual error', or 'odds ratio'"
   }
 
@@ -191,7 +192,6 @@ metric_checks <- function(success_metric) {
     names(outs) <- c("checks_passed", "failed_check")
     return(outs)
   }
-
 }
 
 percent_checks <- function(percent_natural_dispersal) {
@@ -199,8 +199,8 @@ percent_checks <- function(percent_natural_dispersal) {
 
   if (percent_natural_dispersal == 1.0) {
     use_anthropogenic_kernel <- FALSE
-  } else if (percent_natural_dispersal < 1.0  &&
-             percent_natural_dispersal >= 0.0) {
+  } else if (percent_natural_dispersal < 1.0 &&
+    percent_natural_dispersal >= 0.0) {
     use_anthropogenic_kernel <- TRUE
   } else {
     checks_passed <- FALSE
@@ -232,18 +232,20 @@ time_checks <- function(end_date, start_date, time_step, output_frequency) {
   }
 
   if (checks_passed && (!is(end_date, "character") ||
-                        !is(start_date, "character") ||
-                        !is(as.Date(end_date, format = "%Y-%m-%d"), "Date") ||
-                        !is(as.Date(start_date, format = "%Y-%m-%d"), "Date") ||
-                        is.na(as.Date(end_date, format = "%Y-%m-%d")) ||
-                        is.na(as.Date(start_date, format = "%Y-%m-%d")))) {
+    !is(start_date, "character") ||
+    !is(as.Date(end_date, format = "%Y-%m-%d"), "Date") ||
+    !is(as.Date(start_date, format = "%Y-%m-%d"), "Date") ||
+    is.na(as.Date(end_date, format = "%Y-%m-%d")) ||
+    is.na(as.Date(start_date, format = "%Y-%m-%d")))) {
     checks_passed <- FALSE
     failed_check <-
     "End time and/or start time not of type numeric and/or in format YYYY-MM-DD"
   }
 
-  if (checks_passed && !(output_frequency %in% list("week", "month", "day",
-                                                    "year", "time_step"))) {
+  if (checks_passed && !(output_frequency %in% list(
+    "week", "month", "day",
+    "year", "time_step"
+  ))) {
     checks_passed <- FALSE
     failed_check <- "Output frequency must be one of 'week', 'month' or 'day'"
   }
@@ -292,13 +294,13 @@ time_checks <- function(end_date, start_date, time_step, output_frequency) {
     }
 
     if (output_frequency == "year" && time_step == "day" &&
-        number_of_time_steps < 365) {
+      number_of_time_steps < 365) {
       output_frequency <- "final_step"
     } else if (output_frequency == "year" && time_step == "week" &&
-               number_of_time_steps < 52) {
+      number_of_time_steps < 52) {
       output_frequency <- "final_step"
     } else if (output_frequency == "year" && time_step == "month" &&
-               number_of_time_steps < 12) {
+      number_of_time_steps < 12) {
       output_frequency <- "final_step"
     } else {
       output_frequency <- output_frequency
@@ -306,17 +308,20 @@ time_checks <- function(end_date, start_date, time_step, output_frequency) {
   }
 
   if (checks_passed) {
-    outs <- list(checks_passed, number_of_time_steps, number_of_years,
-                 number_of_outputs, output_frequency)
-    names(outs) <- c("checks_passed", "number_of_time_steps",
-                     "number_of_years", "number_of_outputs", "output_frequency")
+    outs <- list(
+      checks_passed, number_of_time_steps, number_of_years,
+      number_of_outputs, output_frequency
+    )
+    names(outs) <- c(
+      "checks_passed", "number_of_time_steps",
+      "number_of_years", "number_of_outputs", "output_frequency"
+    )
     return(outs)
   } else {
     outs <- list(checks_passed, failed_check)
     names(outs) <- c("checks_passed", "failed_check")
     return(outs)
   }
-
 }
 
 # check for making sure priors are in the proper format and output the mean
@@ -336,7 +341,7 @@ prior_checks <- function(priors) {
       start_priors <- priors[[1]]
       sd_priors <- 0
     } else if ((is(priors, "data.frame") | is(priors, "matrix")) &&
-               base::nrow(priors) > 1) {
+      base::nrow(priors) > 1) {
       names(priors) <- c("var", "prob")
       start_priors <- priors$var[priors$prob == max(priors$prob)]
       if (length(start_priors) > 1) {
@@ -375,9 +380,10 @@ bayesian_checks <- function(prior,
   checks_passed <- TRUE
 
   if ((is(prior, "matrix") && nrow(prior) == 1) || (is(prior, "numeric") &&
-                                                    length(prior) == 1)) {
+    length(prior) == 1)) {
     priors <- round(rnorm(count, start_priors, sd_priors) / round_to,
-                    digits = round_to_digits) * round_to
+      digits = round_to_digits
+    ) * round_to
     priors <- as.data.frame(table(priors))
     priors$priors <- as.numeric(as.character(priors$priors))
     priors$prob <- round(priors$Freq / count, digits = 3)
@@ -388,7 +394,7 @@ bayesian_checks <- function(prior,
     priors <- priors[priors$prob > 0.000, ]
     priors <- priors[priors$priors <= bounds[2] & priors$priors >= bounds[1], ]
   } else if ((is(priors, "data.frame") | is(priors, "matrix")) &&
-             nrow(prior) > 1) {
+    nrow(prior) > 1) {
     priors <- prior[, 1:2]
     names(priors) <- c("priors", "prob")
   }
@@ -401,35 +407,44 @@ bayesian_checks <- function(prior,
   calibrated_rates <- base::as.data.frame(table(params))
   calibrated_rates$params <- as.numeric(as.character(calibrated_rates$params))
   calibrated_rates$prob <- round(calibrated_rates$Freq / calibration_count,
-                                 digits = 3)
+    digits = 3
+  )
 
   min_rate <- min(min(priors$priors), min(calibrated_rates$params))
   max_rate <- max(max(priors$priors), max(calibrated_rates$params))
 
-  rates <- data.frame(rate = round(seq(min_rate, max_rate, step_size),
-                                   digits = round_to_digits),
-                      prior_probability = rep(0,
-                                              length(seq(min_rate,
-                                                         max_rate,
-                                                         step_size))),
-                      calibrated_probability =
-                        rep(0, length(seq(min_rate, max_rate, step_size))),
-                      posterior_probability =
-                        rep(0, length(seq(min_rate, max_rate, step_size))))
+  rates <- data.frame(
+    rate = round(seq(min_rate, max_rate, step_size),
+      digits = round_to_digits
+    ),
+    prior_probability = rep(
+      0,
+      length(seq(
+        min_rate,
+        max_rate,
+        step_size
+      ))
+    ),
+    calibrated_probability =
+      rep(0, length(seq(min_rate, max_rate, step_size))),
+    posterior_probability =
+      rep(0, length(seq(min_rate, max_rate, step_size)))
+  )
 
   for (i in seq_len(base::nrow(rates))) {
     if (length(priors$prob[priors$priors == rates$rate[i]]) > 0) {
       rates$prior_probability[i] <- priors$prob[priors$priors == rates$rate[i]]
     }
     if (length(calibrated_rates$prob[calibrated_rates$params ==
-                                     rates$rate[i]]) > 0) {
+      rates$rate[i]]) > 0) {
       rates$calibrated_probability[i] <-
         calibrated_rates$prob[calibrated_rates$params == rates$rate[i]]
     }
   }
   rates$posterior_probability <- round(rates$prior_probability * prior_weight +
-                                         rates$calibrated_probability * weight,
-                                       digits = 3)
+    rates$calibrated_probability * weight,
+  digits = 3
+  )
   posterior_rates <- rates[, c(1, 4)]
 
   if (checks_passed) {
@@ -452,13 +467,13 @@ bayesian_mnn_checks <- function(prior_means,
   } else {
     checks_passed <- FALSE
     failed_check <-
-    "There are not enough prior_means to communte the posterior means for all
+      "There are not enough prior_means to communte the posterior means for all
     paramaters"
   }
 
   if (nrow(prior_cov_matrix) == nrow(calibrated_cov_matrix) &&
-      ncol(prior_cov_matrix) == ncol(calibrated_cov_matrix) &&
-      prior_weight > 0) {
+    ncol(prior_cov_matrix) == ncol(calibrated_cov_matrix) &&
+    prior_weight > 0) {
     posterior_cov_matrix <- prior_cov_matrix * prior_weight +
       calibrated_cov_matrix * weight
   } else if (prior_weight == 0) {
@@ -466,7 +481,7 @@ bayesian_mnn_checks <- function(prior_means,
   } else {
     checks_passed <- FALSE
     failed_check <-
-    "The prior covariance matrix is not the correct dimension to match the
+      "The prior covariance matrix is not the correct dimension to match the
     calibrated covariance matrix in order to computer the posterior covariance
     matrix"
   }
@@ -499,46 +514,46 @@ multispecies_checks <- function(species,
   if (checks_passed && length(species) != length(infected_files)) {
     checks_passed <- FALSE
     failed_check <-
-    "Length of list for species and infected_files must be equal"
+      "Length of list for species and infected_files must be equal"
   }
 
   if (checks_passed && length(reproductive_rate) != length(infected_files)) {
     checks_passed <- FALSE
     failed_check <-
-    "Length of list for infected_files and reproductive_rate must be equal"
+      "Length of list for infected_files and reproductive_rate must be equal"
   }
 
   if (checks_passed && length(percent_natural_dispersal) !=
-      length(infected_files)) {
+    length(infected_files)) {
     checks_passed <- FALSE
     failed_check <-
-    "Length of list for infected_files and percent_natural_dispersal must be
+      "Length of list for infected_files and percent_natural_dispersal must be
     equal"
   }
 
   if (checks_passed && length(natural_kernel_type) != length(infected_files)) {
     checks_passed <- FALSE
     failed_check <-
-    "Length of list for infected_files and natural_kernel_type must be equal"
+      "Length of list for infected_files and natural_kernel_type must be equal"
   }
 
   if (checks_passed && length(anthropogenic_kernel_type) !=
-      length(infected_files)) {
+    length(infected_files)) {
     checks_passed <- FALSE
     failed_check <-
-    "Length of list for infected_files and anthropogenic_kernel_type must be
+      "Length of list for infected_files and anthropogenic_kernel_type must be
     equal"
   }
 
   if (checks_passed && length(natural_distance_scale) !=
-      length(infected_files)) {
+    length(infected_files)) {
     checks_passed <- FALSE
     failed_check <-
     "Length of list for infected_files and natural_distance_scale must be equal"
   }
 
   if (checks_passed && length(anthropogenic_distance_scale) !=
-      length(infected_files)) {
+    length(infected_files)) {
     checks_passed <- FALSE
     failed_check <-
     "Length of list for infected_files and anthropogenic_distance_scale must be
@@ -548,25 +563,25 @@ multispecies_checks <- function(species,
   if (checks_passed && length(natural_dir) != length(infected_files)) {
     checks_passed <- FALSE
     failed_check <-
-    "Length of list for infected_files and natural_dir must be equal"
+      "Length of list for infected_files and natural_dir must be equal"
   }
 
   if (checks_passed && length(natural_kappa) != length(infected_files)) {
     checks_passed <- FALSE
     failed_check <-
-    "Length of list for infected_files and natural_kappa must be equal"
+      "Length of list for infected_files and natural_kappa must be equal"
   }
 
   if (checks_passed && length(anthropogenic_dir) != length(infected_files)) {
     checks_passed <- FALSE
     failed_check <-
-    "Length of list for infected_files and anthropogenic_dir must be equal"
+      "Length of list for infected_files and anthropogenic_dir must be equal"
   }
 
   if (checks_passed && length(anthropogenic_kappa) != length(infected_files)) {
     checks_passed <- FALSE
     failed_check <-
-    "Length of list for infected_files and anthropogenic_kappa must be equal"
+      "Length of list for infected_files and anthropogenic_kappa must be equal"
   }
 
   if (checks_passed) {
@@ -596,28 +611,33 @@ movement_checks <- function(x, rast, start_date, end_date) {
   if (checks_passed) {
     moves <- read.csv(x, header = TRUE)
     movement_from <- SpatialPointsDataFrame(moves[, 1:2],
-                                            data = moves,
-                                            proj4string =
-                                              CRS("+init=epsg:4326"))
-    movement_to <- SpatialPointsDataFrame(moves[, 3:4], data = moves,
-                                          proj4string = CRS("+init=epsg:4326"))
+      data = moves,
+      proj4string =
+        CRS("+init=epsg:4326")
+    )
+    movement_to <- SpatialPointsDataFrame(moves[, 3:4],
+      data = moves,
+      proj4string = CRS("+init=epsg:4326")
+    )
     movement_from <- spTransform(movement_from, CRSobj = crs(rast))
     movement_to <- spTransform(movement_to, CRSobj = crs(rast))
-    cell_from <-  raster::extract(rast, movement_from, cellnumbers = TRUE)
-    cell_to <-  raster::extract(rast, movement_to, cellnumbers = TRUE)
+    cell_from <- raster::extract(rast, movement_from, cellnumbers = TRUE)
+    cell_to <- raster::extract(rast, movement_to, cellnumbers = TRUE)
     rowcol_from <- rowColFromCell(rast, cell_from[, 1])
     rowcol_to <- rowColFromCell(rast, cell_to[, 1])
-    movements <- data.frame(row_from = rowcol_from[, 1],
-                            col_from = rowcol_from[, 2],
-                            row_to = rowcol_to[, 1], col_to = rowcol_to[, 2],
-                            num_animals = moves$animals,
-                            date = moves$date)
+    movements <- data.frame(
+      row_from = rowcol_from[, 1],
+      col_from = rowcol_from[, 2],
+      row_to = rowcol_to[, 1], col_to = rowcol_to[, 2],
+      num_animals = moves$animals,
+      date = moves$date
+    )
     movements <- movements[!is.na(movements$row_from) &
-                             !is.na(movements$col_from) &
-                             !is.na(movements$row_to) &
-                             !is.na(movements$col_to) &
-                             !is.na(movements$num_animals) &
-                             !is.na(movements$date), ]
+      !is.na(movements$col_from) &
+      !is.na(movements$row_to) &
+      !is.na(movements$col_to) &
+      !is.na(movements$num_animals) &
+      !is.na(movements$date), ]
     movements <- movements[movements$num_animals > 0, ]
     movements$date <- paste("'", movements$date, "'", sep = "")
     movements$date <- lubridate::mdy(movements$date)
@@ -667,8 +687,10 @@ parameter_checks <- function(n, parameter_means, parameter_cov_matrix) {
   }
 
   if (checks_passed) {
-    parameters <- data.frame(MASS::mvrnorm(n, parameter_means,
-                                           parameter_cov_matrix))
+    parameters <- data.frame(MASS::mvrnorm(
+      n, parameter_means,
+      parameter_cov_matrix
+    ))
   }
 
   if (checks_passed) {
