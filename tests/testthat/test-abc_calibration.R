5355da563818ae75bc4de821ae6b70605451cc7c
ChrisJones687 <cjones1688@gmail.com>
2020-09-13 06:38:10 -0400

    linted tests

--- tests/testthat/test-abc_calibration.R
+++ tests/testthat/test-abc_calibration.R
@@ -1,30 +1,37 @@
 context("test-abc_calibration")
 
-test_that("ABC calibration has correctly formatted returns with multiple output comparisons", {
+test_that("ABC calibration has correctly formatted returns with multiple output
+          comparisons", {
   skip_on_appveyor()
-  infected_years_file <- system.file("extdata", "simple20x20", "infected_years.tif", package = "PoPS")
-  number_of_observations <- 68    ### This is the number of infected cells - just make sure it's consistent across years
+  infected_years_file <-
+    system.file("extdata", "simple20x20", "infected_years.tif",
+                package = "PoPS")
+  number_of_observations <- 68
   prior_number_of_observations <- 0
-  prior_means <- c(0, 0, 0, 0, 0, 0)    ### leave as 0 for now, means that you are giving them no weight
+  prior_means <- c(0, 0, 0, 0, 0, 0)
   prior_cov_matrix <- matrix(ncol = 6, nrow = 6, 0)
-  params_to_estimate <- c(T, T, T, T, F, F)  ### 1st: reproductive rates, 2nd: natural distance, 3rd: percent natural, 4tH: anthropogenic distance, 5th Natural Kappa, 6th anthropogenic kappa
+  params_to_estimate <- c(T, T, T, T, F, F)
   number_of_generations <- 4
   generation_size <- 10
-  checks = c(1200, 100000, 900, 1000)
-  infected_file <- system.file("extdata", "simple20x20", "initial_infection.tif", package = "PoPS")
-  host_file <- system.file("extdata", "simple20x20", "host.tif", package = "PoPS")
-  total_populations_file <- system.file("extdata", "simple20x20", "all_plants.tif", package = "PoPS")
+  checks <- c(1200, 100000, 900, 1000)
+  infected_file <-
+    system.file("extdata", "simple20x20", "initial_infection.tif",
+                package = "PoPS")
+  host_file <-
+    system.file("extdata", "simple20x20", "host.tif", package = "PoPS")
+  total_populations_file <-
+    system.file("extdata", "simple20x20", "all_plants.tif", package = "PoPS")
   temp <- FALSE
   temperature_coefficient_file <- ""
   precip <- FALSE
   precipitation_coefficient_file <- ""
-  model_type = "SEI"
-  latency_period = 14
-  time_step <- "day"
+  model_type <- "SEI"
+  latency_period <- 14
+  time_step <- "week"
   season_month_start <- 1
   season_month_end <- 12
-  start_date <- '2003-01-01'
-  end_date <- '2003-02-11'
+  start_date <- "2003-01-01"
+  end_date <- "2003-02-11"
   use_lethal_temperature <- FALSE
   temperature_file <- ""
   lethal_temperature <- -30
@@ -33,7 +40,7 @@ test_that("ABC calibration has correctly formatted returns with multiple output 
   mortality_rate <- 0
   mortality_time_lag <- 0
   management <- FALSE
-  treatment_dates <- c('2003-01-24')
+  treatment_dates <- c("2003-01-24")
   treatments_file <- ""
   treatment_method <- "ratio"
   natural_kernel_type <- "exponential"
@@ -47,15 +54,29 @@ test_that("ABC calibration has correctly formatted returns with multiple output 
   mask <- NULL
   success_metric <- "number of locations and total distance"
   output_frequency <- "week"
-  movements_file = ""
-  use_movements = FALSE
+  output_frequency_n <- 1
+  movements_file <- ""
+  use_movements <- FALSE
   percent_natural_dispersal <- 1.0
   anthropogenic_distance_scale <- 0.0
+  number_of_iterations <- 10
+  number_of_cores <- 2
+  start_exposed <- FALSE
+  generate_stochasticity <- TRUE
+  establishment_stochasticity <- TRUE
+  movement_stochasticity <- TRUE
+  deterministic <- FALSE
+  establishment_probability <- 0.5
+  dispersal_percentage <- 0.99
+  quarantine_areas_file <- ""
+  use_quarantine <- FALSE
+  output_frequency_n <- 1
+  use_spreadrates <- FALSE
 
   data <- abc_calibration(infected_years_file,
                           number_of_observations,
                           prior_number_of_observations,
-                          prior_means, 
+                          prior_means,
                           prior_cov_matrix,
                           params_to_estimate,
                           number_of_generations,
@@ -97,7 +118,7 @@ test_that("ABC calibration has correctly formatted returns with multiple output 
                           mask,
                           success_metric,
                           output_frequency,
-                          output_frequency_n =1,
+                          output_frequency_n = 1,
                           movements_file,
                           use_movements,
                           start_exposed = FALSE,
@@ -119,117 +140,154 @@ test_that("ABC calibration has correctly formatted returns with multiple output 
   expect_equal(ncol(data$posterior_cov_matrix), 6)
   expect_type(data$total_number_of_observations, "double")
   expect_equal(data$total_number_of_observations, number_of_observations)
-  expect_equal(nrow(data$raw_calibration_data), number_of_generations*generation_size)
+  expect_equal(nrow(data$raw_calibration_data),
+               number_of_generations * generation_size)
 
-})
+          })
 
 
-# test_that("ABC calibration has correctly formatted returns and runs with a single output comparison", {
-#   infected_years_file <- system.file("extdata", "simple20x20", "infected_single.tif", package = "PoPS")
-#   number_of_observations <- 68    ### This is the number of infected cells - just make sure it's consistent across years 
-#   prior_number_of_observations <- 0
-#   prior_means <- c(0, 0, 0, 0, 0, 0)    ### leave as 0 for now, means that you are giving them no weight
-#   prior_cov_matrix <- matrix(ncol = 6, nrow = 6, 0)
-#   params_to_estimate <- c(T, T, T, T, F, F)  ### 1st: reproductive rates, 2nd: natural distance, 3rd: percent natural, 4tH: anthropogenic distance, 5th Natural Kappa, 6th anthropogenic kappa
-#   number_of_generations <- 6
-#   generation_size <- 10
-#   checks = c(1200, 90000, 900, 1000)
-#   infected_file <- system.file("extdata", "simple20x20", "initial_infection.tif", package = "PoPS")
-#   host_file <- system.file("extdata", "simple20x20", "host.tif", package = "PoPS")
-#   total_populations_file <- system.file("extdata", "simple20x20", "all_plants.tif", package = "PoPS")
-#   temp <- FALSE
-#   temperature_coefficient_file <- ""
-#   precip <- FALSE
-#   precipitation_coefficient_file <- ""
-#   model_type = "SI"
-#   latency_period = 0
-#   time_step <- "month"
-#   season_month_start <- 1
-#   season_month_end <- 12
-#   start_date <- '2003-01-01'
-#   end_date <- '2003-12-31'
-#   use_lethal_temperature <- FALSE
-#   temperature_file <- ""
-#   lethal_temperature <- -30
-#   lethal_temperature_month <- 1
-#   mortality_on <- FALSE
-#   mortality_rate <- 0
-#   mortality_time_lag <- 0
-#   management <- FALSE
-#   treatment_dates <- c('2003-01-24')
-#   treatments_file <- ""
-#   treatment_method <- "ratio"
-#   natural_kernel_type <- "exponential"
-#   anthropogenic_kernel_type <- "cauchy"
-#   natural_dir <- "NONE"
-#   natural_kappa <- 0
-#   anthropogenic_dir <- "NONE"
-#   anthropogenic_kappa <- 0
-#   pesticide_duration <- c(0)
-#   pesticide_efficacy <- 1.0
-#   mask <- NULL
-#   success_metric <- "number of locations and total distance"
-#   output_frequency <- "year"
-#   movements_file = ""
-#   use_movements = FALSE
-#   percent_natural_dispersal <- 1.0
-#   anthropogenic_distance_scale <- 0.0
-#   
-#   data <- abc_calibration(infected_years_file, 
-#                           number_of_observations, 
-#                           prior_number_of_observations,
-#                           prior_means, prior_cov_matrix, 
-#                           params_to_estimate,
-#                           number_of_generations,
-#                           generation_size,
-#                           checks,
-#                           infected_file, 
-#                           host_file, 
-#                           total_populations_file, 
-#                           temp, 
-#                           temperature_coefficient_file, 
-#                           precip, 
-#                           precipitation_coefficient_file,
-#                           model_type,
-#                           latency_period,
-#                           time_step, 
-#                           season_month_start, 
-#                           season_month_end, 
-#                           start_date, 
-#                           end_date, 
-#                           use_lethal_temperature, 
-#                           temperature_file,
-#                           lethal_temperature, 
-#                           lethal_temperature_month,
-#                           mortality_on, 
-#                           mortality_rate, 
-#                           mortality_time_lag, 
-#                           management, 
-#                           treatment_dates, 
-#                           treatments_file,
-#                           treatment_method,
-#                           natural_kernel_type, 
-#                           anthropogenic_kernel_type,
-#                           natural_dir, 
-#                           natural_kappa, 
-#                           anthropogenic_dir, 
-#                           anthropogenic_kappa,
-#                           pesticide_duration, 
-#                           pesticide_efficacy,
-#                           mask, 
-#                           success_metric, 
-#                           output_frequency,
-#                           movements_file, 
-#                           use_movements)
-#   
-#   expect_length(data$posterior_means, 6)
-#   expect_vector(data$posterior_means, ptype = double(), size = 6)
-#   expect_is(data$posterior_cov_matrix, class = "matrix")
-#   expect_type(data$posterior_cov_matrix, "double")
-#   expect_equal(nrow(data$posterior_cov_matrix), 6)
-#   expect_equal(ncol(data$posterior_cov_matrix), 6)
-#   expect_type(data$total_number_of_observations, "double")
-#   expect_equal(data$total_number_of_observations, number_of_observations)
-#   expect_equal(nrow(data$raw_calibration_data), number_of_generations*generation_size)
-#   
-# })
+test_that("ABC calibration has correctly formatted returns and runs with a
+          single output comparison", {
+            infected_years_file <-
+              system.file("extdata", "simple20x20", "infected_single.tif",
+                          package = "PoPS")
+            number_of_observations <- 68
+            prior_number_of_observations <- 0
+            prior_means <- c(0, 0, 0, 0, 0, 0)
+            prior_cov_matrix <- matrix(ncol = 6, nrow = 6, 0)
+            params_to_estimate <- c(T, T, T, T, F, F)
+            number_of_generations <- 4
+            generation_size <- 10
+            checks <- c(1200, 90000, 900, 1000)
+            infected_file <-
+              system.file("extdata", "simple20x20", "initial_infection.tif",
+                          package = "PoPS")
+            host_file <-
+              system.file("extdata", "simple20x20", "host.tif",
+                          package = "PoPS")
+            total_populations_file <-
+              system.file("extdata", "simple20x20", "all_plants.tif",
+                          package = "PoPS")
+            temp <- FALSE
+            temperature_coefficient_file <- ""
+            precip <- FALSE
+            precipitation_coefficient_file <- ""
+            model_type <- "SI"
+            latency_period <- 0
+            time_step <- "month"
+            season_month_start <- 1
+            season_month_end <- 12
+            start_date <- "2003-01-01"
+            end_date <- "2003-12-31"
+            use_lethal_temperature <- FALSE
+            temperature_file <- ""
+            lethal_temperature <- -30
+            lethal_temperature_month <- 1
+            mortality_on <- FALSE
+            mortality_rate <- 0
+            mortality_time_lag <- 0
+            management <- FALSE
+            treatment_dates <- c("2003-01-24")
+            treatments_file <- ""
+            treatment_method <- "ratio"
+            natural_kernel_type <- "exponential"
+            anthropogenic_kernel_type <- "cauchy"
+            natural_dir <- "NONE"
+            natural_kappa <- 0
+            anthropogenic_dir <- "NONE"
+            anthropogenic_kappa <- 0
+            pesticide_duration <- c(0)
+            pesticide_efficacy <- 1.0
+            mask <- NULL
+            success_metric <- "number of locations and total distance"
+            output_frequency <- "year"
+            movements_file <- ""
+            use_movements <- FALSE
+            percent_natural_dispersal <- 1.0
+            anthropogenic_distance_scale <- 0.0
+            number_of_iterations <- 10
+            number_of_cores <- 2
+            start_exposed <- FALSE
+            generate_stochasticity <- TRUE
+            establishment_stochasticity <- TRUE
+            movement_stochasticity <- TRUE
+            deterministic <- FALSE
+            establishment_probability <- 0.5
+            dispersal_percentage <- 0.99
+            quarantine_areas_file <- ""
+            use_quarantine <- FALSE
+            output_frequency_n <- 1
+            use_spreadrates <- FALSE
+
+            data <- abc_calibration(infected_years_file,
+                                    number_of_observations,
+                                    prior_number_of_observations,
+                                    prior_means,
+                                    prior_cov_matrix,
+                                    params_to_estimate,
+                                    number_of_generations,
+                                    generation_size,
+                                    checks,
+                                    infected_file,
+                                    host_file,
+                                    total_populations_file,
+                                    temp,
+                                    temperature_coefficient_file,
+                                    precip,
+                                    precipitation_coefficient_file,
+                                    model_type,
+                                    latency_period,
+                                    time_step,
+                                    season_month_start,
+                                    season_month_end,
+                                    start_date,
+                                    end_date,
+                                    use_lethal_temperature,
+                                    temperature_file,
+                                    lethal_temperature,
+                                    lethal_temperature_month,
+                                    mortality_on,
+                                    mortality_rate,
+                                    mortality_time_lag,
+                                    management,
+                                    treatment_dates,
+                                    treatments_file,
+                                    treatment_method,
+                                    natural_kernel_type,
+                                    anthropogenic_kernel_type,
+                                    natural_dir,
+                                    natural_kappa,
+                                    anthropogenic_dir,
+                                    anthropogenic_kappa,
+                                    pesticide_duration,
+                                    pesticide_efficacy,
+                                    mask,
+                                    success_metric,
+                                    output_frequency,
+                                    output_frequency_n = 1,
+                                    movements_file,
+                                    use_movements,
+                                    start_exposed = FALSE,
+                                    generate_stochasticity = TRUE,
+                                    establishment_stochasticity = TRUE,
+                                    movement_stochasticity = TRUE,
+                                    deterministic = FALSE,
+                                    establishment_probability = 0.5,
+                                    dispersal_percentage = 0.99,
+                                    quarantine_areas_file = "",
+                                    use_quarantine = FALSE,
+                                    use_spreadrates = FALSE)
+
+            expect_length(data$posterior_means, 6)
+            expect_vector(data$posterior_means, ptype = double(), size = 6)
+            expect_is(data$posterior_cov_matrix, class = "matrix")
+            expect_type(data$posterior_cov_matrix, "double")
+            expect_equal(nrow(data$posterior_cov_matrix), 6)
+            expect_equal(ncol(data$posterior_cov_matrix), 6)
+            expect_type(data$total_number_of_observations, "double")
+            expect_equal(data$total_number_of_observations,
+                         number_of_observations)
+            expect_equal(nrow(data$raw_calibration_data),
+                         number_of_generations * generation_size)
+
+          })
--- tests/testthat/test-quantity_allocation_disagreement.R
+++ tests/testthat/test-quantity_allocation_disagreement.R
@@ -1,68 +1,81 @@
 context("test-quantity_allocation_disagreement")
 
-test_that("Quantity allocation is 0 when comparison and reference are the exact same!", {
+test_that(
+  "Quantity allocation is 0 when comparison and reference are the exact
+  same!", {
   comp <- raster(matrix(1, nrow = 2, ncol = 2))
   ref <- raster(matrix(1, nrow = 2, ncol = 2))
   data <- quantity_allocation_disagreement(ref, comp)
   expect_equal(data$quantity_disagreement, 0)
   expect_equal(data$allocation_disagreement, 0)
-  expect_equal(data$total_disagreement,0)
+  expect_equal(data$total_disagreement, 0)
   expect_equal(data$omission, 0)
   expect_equal(data$commission, 0)
   expect_equal(data$odds_ratio, 0)
 })
 
-test_that("Check that quantity disagreement, total disagreement, ommision, and directional disagreement are 4 when ref is all 1's and comp is all 0's!", {
+test_that("Check that quantity disagreement, total disagreement, ommision, and
+          directional disagreement are 4 when ref is all 1's and comp is all
+          0's!", {
   comp <- raster(matrix(0, nrow = 2, ncol = 2))
   ref <- raster(matrix(1, nrow = 2, ncol = 2))
   data <- quantity_allocation_disagreement(ref, comp)
   expect_equal(data$quantity_disagreement, 4)
   expect_equal(data$allocation_disagreement, 0)
-  expect_equal(data$total_disagreement,4)
+  expect_equal(data$total_disagreement, 4)
   expect_equal(data$omission, 4)
   expect_equal(data$commission, 0)
   expect_equal(data$odds_ratio, 0)
 })
 
-test_that("Check that quantity disagreement, total disagreement, number_of_infected_comp, and commision are 4 and directional disagreement is -4 when ref is all 0's and comp is all 1's!", {
+test_that("Check that quantity disagreement, total disagreement,
+          number_of_infected_comp, and commision are 4 and directional
+          disagreement is -4 when ref is all 0's and comp is all 1's!", {
   comp <- raster(matrix(1, nrow = 2, ncol = 2))
   ref <- raster(matrix(0, nrow = 2, ncol = 2))
   data <- quantity_allocation_disagreement(ref, comp)
   expect_equal(data$quantity_disagreement, 4)
   expect_equal(data$allocation_disagreement, 0)
-  expect_equal(data$total_disagreement,4)
+  expect_equal(data$total_disagreement, 4)
   expect_equal(data$omission, 0)
   expect_equal(data$commission, 4)
   expect_equal(data$odds_ratio, 0)
 })
 
-test_that("Check that allocation disgreement and total disagreement are 4 and number_of_infected_comp, ommision and commision are 4 and directional disagreement is 0 when ref has 1's at [2,1] and [2,2] and comp  1's at [1,1] and [1,2]!", {
+test_that("Check that allocation disgreement and total disagreement are 4 and
+          number_of_infected_comp, ommision and commision are 4 and directional
+          disagreement is 0 when ref has 1's at [2,1] and [2,2] and comp  1's
+          at [1,1] and [1,2]!", {
   comp <- raster(matrix(0, nrow = 2, ncol = 2))
-  comp[1,1] <- 1
-  comp[1,2] <- 1
+  comp[1, 1] <- 1
+  comp[1, 2] <- 1
   ref <- raster(matrix(0, nrow = 2, ncol = 2))
-  ref[2,1] <- 1
-  ref[2,2] <- 1
+  ref[2, 1] <- 1
+  ref[2, 2] <- 1
   data <- quantity_allocation_disagreement(ref, comp)
   expect_equal(data$quantity_disagreement, 0)
   expect_equal(data$allocation_disagreement, 4)
-  expect_equal(data$total_disagreement,4)
+  expect_equal(data$total_disagreement, 4)
   expect_equal(data$omission, 2)
   expect_equal(data$commission, 2)
   expect_equal(data$odds_ratio, 0)
 })
 
-test_that("Check that allocation disgreement and total disagreement are 4 and number_of_infected_comp, ommision and commision are 4 and directional disagreement is 0 when ref has 1's at [2,1] and [2,2] and comp  1's at [1,1] and [1,2]!", {
+test_that(
+  "Check that allocation disgreement and total disagreement are 4 and
+  number_of_infected_comp, ommision and commision are 4 and directional
+  disagreement is 0 when ref has 1's at [2,1] and [2,2] and comp  1's at [1,1]
+  and [1,2]!", {
   comp <- raster(matrix(0, nrow = 2, ncol = 2))
-  comp[2,1] <- 1
-  comp[1,2] <- 1
+  comp[2, 1] <- 1
+  comp[1, 2] <- 1
   ref <- raster(matrix(0, nrow = 2, ncol = 2))
-  ref[2,1] <- 1
-  ref[2,2] <- 1
+  ref[2, 1] <- 1
+  ref[2, 2] <- 1
   data <- quantity_allocation_disagreement(ref, comp)
   expect_equal(data$quantity_disagreement, 0)
   expect_equal(data$allocation_disagreement, 2)
-  expect_equal(data$total_disagreement,2)
+  expect_equal(data$total_disagreement, 2)
   expect_equal(data$omission, 1)
   expect_equal(data$commission, 1)
   expect_equal(data$odds_ratio, 1)
@@ -71,43 +84,47 @@ test_that("Check that allocation disgreement and total disagreement are 4 and nu
 
 test_that("Check that configuration disagreement works", {
   comp <- raster(matrix(0, nrow = 2, ncol = 2))
-  comp[2,1] <- 1
-  comp[1,2] <- 1
+  comp[2, 1] <- 1
+  comp[1, 2] <- 1
   ref <- raster(matrix(0, nrow = 2, ncol = 2))
-  ref[2,1] <- 1
-  ref[1,2] <- 1
+  ref[2, 1] <- 1
+  ref[1, 2] <- 1
   data <- quantity_allocation_disagreement(ref, comp, configuration = TRUE)
   expect_equal(data$quantity_disagreement, 0)
   expect_equal(data$allocation_disagreement, 0)
-  expect_equal(data$total_disagreement,0)
+  expect_equal(data$total_disagreement, 0)
   expect_equal(data$omission, 0)
   expect_equal(data$commission, 0)
   expect_equal(data$odds_ratio, 4)
   expect_equal(data$configuration_disagreement, 0)
-  
+
   comp <- raster(matrix(0, nrow = 2, ncol = 2))
-  comp[2,1] <- 1
-  comp[1,2] <- 1
+  comp[2, 1] <- 1
+  comp[1, 2] <- 1
   ref <- raster(matrix(0, nrow = 2, ncol = 2))
-  ref[2,1] <- 1
-  ref[2,2] <- 1
+  ref[2, 1] <- 1
+  ref[2, 2] <- 1
   mask <- raster(matrix(0, nrow = 2, ncol = 2))
-  mask[1,2] <- NA
-  mask[2,2] <- NA
-  data <- quantity_allocation_disagreement(ref, comp, configuration = TRUE, mask = mask)
+  mask[1, 2] <- NA
+  mask[2, 2] <- NA
+  data <-
+    quantity_allocation_disagreement(ref,
+                                     comp,
+                                     configuration = TRUE,
+                                     mask = mask)
   expect_equal(data$quantity_disagreement, 0)
   expect_equal(data$allocation_disagreement, 0)
-  expect_equal(data$total_disagreement,0)
+  expect_equal(data$total_disagreement, 0)
   expect_equal(data$omission, 0)
   expect_equal(data$commission, 0)
   expect_equal(data$odds_ratio, 1)
   expect_equal(data$configuration_disagreement, 0)
-  
-  
+
+
   data <- quantity_allocation_disagreement(ref, comp, configuration = TRUE)
   expect_equal(data$quantity_disagreement, 0)
   expect_equal(data$allocation_disagreement, 2)
-  expect_equal(data$total_disagreement,2)
+  expect_equal(data$total_disagreement, 2)
   expect_equal(data$omission, 1)
   expect_equal(data$commission, 1)
   expect_equal(data$odds_ratio, 1)
