209970cb067fa7ed4713f1cdd55f819f7abad89b
ChrisJones687 <cjones1688@gmail.com>
2020-10-16 16:13:57 -0400

    added tests for multispecies for config function

--- tests/testthat/test-configuration.R
+++ tests/testthat/test-configuration.R
@@ -85,6 +85,16 @@ test_that("Configuration returns proper values when no errors present", {
   config$function_name <- "multirun"
   config <- configuration(config)
   expect_equal(config$failure, NULL)
+
+  config$infected_files <-
+    c(system.file("extdata", "simple20x20", "initial_infection.tif",
+                package = "PoPS"))
+  config$parameter_means <- list(c(0.2, 20, 0.99, 6000, 0, 0))
+  config$parameter_cov_matrix <- list(matrix(ncol = 6, nrow = 6, 0.1))
+  config$function_name <- "auto-manage"
+  config$species <- c("pest")
+  config <- configuration(config)
+  expect_equal(config$failure, NULL)
 })
 
 test_that("configuration returns proper errors", {
