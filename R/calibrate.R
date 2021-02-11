910abaccc8c9a9655eab7603a92b9d0e0e95357d
ChrisJones687 <cjones1688@gmail.com>
2021-02-11 08:22:53 -0500

    updated calibrate function for terra package

--- R/calibrate.R
+++ R/calibrate.R
@@ -61,9 +61,9 @@
 #' the calibration to converge (recommend a minimum of at least 100,000 but
 #' preferably 1 million).
 #'
-#' @importFrom raster raster values as.matrix xres yres stack reclassify
-#' cellStats nlayers extent extension compareCRS getValues calc extract
-#' rasterToPoints pointDistance rowFromCell colFromCell
+#' @importFrom terra global rast xres yres classify extract ext as.points ncol
+#' nrow nlyr rowFromCell colFromCell values as.matrix rowFromCell colFromCell
+#' crs app
 #' @importFrom stats runif rnorm cov
 #' @importFrom doParallel registerDoParallel
 #' @importFrom foreach  registerDoSEQ %dopar% %do% %:% foreach
@@ -321,7 +321,7 @@ calibrate <- function(infected_years_file,
     infected_data_points <-
       vector(mode = "list", length = config$number_of_outputs)
 
-    for (y in seq_len(nlayers(config$infection_years))) {
+    for (y in seq_len(terra::nlyr(config$infection_years))) {
       inf_year <- config$infection_years[[y]]
       num_infected_data[[y]] <- sum(inf_year[inf_year > 0])
       num_locs_data[[y]] <- sum(inf_year[inf_year > 0] > 0)
@@ -331,14 +331,14 @@ calibrate <- function(infected_years_file,
             "number of locations, number of infections, and total distance"
           )) {
         infected_data_points[[y]] <-
-          rasterToPoints(inf_year, fun = function(x) {
-            x > 0
-          }, spatial = TRUE)
-      }
+          terra::as.points(inf_year, crs = terra::crs(inf_year),
+                           fun = function(x) { x > 0},
+                           spatial = TRUE)
+        }
     }
 
     # calculate total infections per output in the landscape
-    total_infections <- cellStats(config$infection_years, "sum")
+    total_infections <- terra::global(config$infection_years, "sum")
     if (length(total_infections) > config$number_of_outputs) {
       total_infections <- total_infections[1:config$number_of_outputs]
     }
@@ -440,12 +440,12 @@ calibrate <- function(infected_years_file,
         # calculate comparison metrics for simulation data for each time step in
         # the simulation
         ## To DO add in configuration metrics to this)
-        for (y in seq_len(nlayers(config$infection_years))) {
-          if (nlayers(config$infection_years) > 1) {
-            infected_sims[[y]][] <- data$infected[[y]]
-            infected_sim[] <- data$infected[[y]]
+        for (y in seq_len(terra::nlyr(config$infection_years))) {
+          if (terra::nlyr(config$infection_years) > 1) {
+            terra::values(infected_sims[[y]]) <- data$infected[[y]]
+            terra::values(infected_sim) <- data$infected[[y]]
           } else {
-            infected_sim[] <- data$infected[[y]]
+            terra::values(infected_sim) <- data$infected[[y]]
           }
 
           if (!is.null(config$mask)) {
@@ -467,17 +467,15 @@ calibrate <- function(infected_years_file,
               )
           ) {
             infected_sim_points[[y]] <-
-              rasterToPoints(infected_sim,
+              terra::as.points(infected_sim, crs = crs(infected_sim),
                              fun = function(x) {
                                x > 0
                              },
                              spatial = TRUE
               )
             dist[[y]] <-
-              pointDistance(infected_sim_points[[y]],
-                            infected_data_points[[y]],
-                            lonlat = FALSE
-              )
+              terra::distance(infected_sim_points[[y]],
+                            infected_data_points[[y]])
             if (class(dist) == "matrix") {
               distance_differences[[y]] <- apply(dist[[y]], 2, min)
             } else {
@@ -504,7 +502,8 @@ calibrate <- function(infected_years_file,
           distance_differences <- 0
         }
 
-        number_infected_differences <- sqrt((num_infected_data - num_infected_simulated)^2)
+        number_infected_differences <-
+          sqrt((num_infected_data - num_infected_simulated)^2)
         location_differences <- sqrt((num_locs_data - num_locs_simulated)^2)
 
         number_infected_difference <- sum(number_infected_differences)
@@ -565,8 +564,7 @@ calibrate <- function(infected_years_file,
           paste("The current generation is ", config$current_bin, " and the
             current particle is ", config$current_particles,
                 " and the current acceptance rate is ", acceptance_rate,
-                sep = ""
-          )
+                sep = "")
         print(acceptance_rate_info)
       }
 
@@ -631,15 +629,15 @@ calibrate <- function(infected_years_file,
         proposed_anthropogenic_kappa
       )
 
-    comp_year <- raster::raster(config$infected_file)
+    comp_year <- terra::rast(config$infected_file)
     all_disagreement <-
       foreach::foreach(
         q = seq_len(length(data$infected)),
         .combine = rbind,
-        .packages = c("raster", "PoPS"),
+        .packages = c("terra", "PoPS"),
         .final = colSums
       ) %do% {
-        comp_year[] <- data$infected[[q]]
+        terra::values(comp_year) <- data$infected[[q]]
         quantity_allocation_disagreement(
           config$infection_years[[q]],
           comp_year,
@@ -722,7 +720,7 @@ calibrate <- function(infected_years_file,
                   digits = 0)
         }
       } else {
-        proposed_anthropogenic_distance_scale <- 0
+        proposed_anthropogenic_distance_scale <- 0.1
       }
 
       if (config$params_to_estimate[5]) {
@@ -760,15 +758,15 @@ calibrate <- function(infected_years_file,
         )
 
       # set up comparison
-      comp_year <- raster::raster(config$infected_file)
+      comp_year <- terra::rast(config$infected_file)
       all_disagreement <-
         foreach::foreach(
           q = seq_len(length(data$infected)),
           .combine = rbind,
-          .packages = c("raster", "PoPS"),
+          .packages = c("terra", "PoPS"),
           .final = colSums
         ) %do% {
-          comp_year[] <- data$infected[[q]]
+          terra::values(comp_year) <- data$infected[[q]]
           quantity_allocation_disagreement(
             config$infection_years[[q]],
             comp_year,
@@ -913,6 +911,5 @@ calibrate <- function(infected_years_file,
       "posterior_means", "posterior_cov_matrix",
       "total_number_of_observations", "raw_calibration_data"
     )
-
   return(outputs)
 }
