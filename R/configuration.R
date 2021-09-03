66be618f392f6dd47e537d948f97dd426906c4b5
ChrisJones687 <cjones1688@gmail.com>
2021-09-03 16:36:02 -0400

    removed success metrics check

--- R/configuration.R
+++ R/configuration.R
@@ -731,18 +731,6 @@ configuration <- function(config) {
     config$infection_years2 <- infection_years2
   }
 
-  if (config$function_name %in% c("validate") |
-    (config$function_name %in% c("calibrate") &&
-      config$calibration_method == "MCMC")) {
-    metric_check <- metric_checks(config$success_metric)
-    if (metric_check$checks_passed) {
-      config$configuration <- metric_check$configuration
-    } else {
-      config$failure <- metric_check$failed_check
-      return(config)
-    }
-  }
-
   if (config$function_name %in% c("calibrate") &&
     config$calibration_method == "ABC") {
     config$num_particles <-
