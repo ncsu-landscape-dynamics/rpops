9eba954bd5345efab20bf0a389fa0582931afab5
ChrisJones687 <cjones1688@gmail.com>
2022-06-09 10:18:56 -0400

    added survival rates to pops_model documentation

--- R/pops_model.R
+++ R/pops_model.R
@@ -70,7 +70,7 @@
 #' anthropogenic_kernel_type = 'network'.
 #' @param network_max_distance maximum time a propagule rides on the network. Used if
 #' anthropogenic_kernel_type = 'network'.
-#' #' @param survival_rates vector of matrices of survival rates used to determine percentage of
+#' @param survival_rates vector of matrices of survival rates used to determine percentage of
 #' overwinter population that emerges
 #'
 #' @return list of vector matrices of infected and susceptible hosts per
