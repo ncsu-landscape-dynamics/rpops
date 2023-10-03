5927e011718685dbc1f01e6587576e20f178260f
Chris Jones <cjones1688@gmail.com>
2023-10-03 06:28:27 -0400

    R seeds write (#179)
    * added  write out random_seeds file in validation and multirun functions
    
    * add write all simulations and tests for writing out random_seeds and all_sims
    
    * fix lint issue

--- R/pops_multirun.R
+++ R/pops_multirun.R
@@ -11,12 +11,12 @@
 #' forecast spread of the pest/pathogen into the future.
 #'
 #' @inheritParams pops
-#' @param number_of_iterations how many iterations do you want to run to allow
-#' the calibration to converge at least 10
-#' @param number_of_cores enter how many cores you want to use (default = NA).
-#' If not set uses the # of CPU cores - 1. must be an integer >= 1
-#' @param write_outputs Either c("summary_outputs", or
-#' "None"). If not "None" output folder path must be provided.
+#' @param number_of_iterations how many iterations do you want to run to allow the calibration to
+#' converge at least 10
+#' @param number_of_cores enter how many cores you want to use (default = NA). If not set uses the
+#' # of CPU cores - 1. must be an integer >= 1
+#' @param write_outputs Either c("summary_outputs", "all_simulations", or "None"). If not
+#' "None" output folder path must be provided.
 #' @param output_folder_path this is the full path with either / or \\ (e.g.,
 #' "C:/user_name/desktop/pops_sod_2020_2023/outputs/")
 #'
@@ -202,6 +202,11 @@ pops_multirun <- function(infected_file,
     stop(config$failure)
   }
 
+  if (config$multiple_random_seeds && is.null(config$file_random_seeds) &&
+      dir.exists(config$output_folder_path)) {
+    write.csv(config$random_seeds, paste0(config$output_folder_path, "forecast_random_seeds.csv"))
+  }
+
   config$crs <- terra::crs(config$host)
 
   i <- NULL
@@ -349,29 +354,29 @@ pops_multirun <- function(infected_file,
       run$quarantine_escape_direction <- data$quarantine_escape_directions
       run$exposed_runs <- data$exposed
 
-      # if (config$write_outputs == "all_simulations") {
-      #   infected_out <- terra::rast(config$infected_file)
-      #   susectible_out <- terra::rast(config$infected_file)
-      #   exposed_out <- terra::rast(config$infected_file)
-      #   for (q in seq_len(length(data$infected))) {
-      #     values(infected_out[[q]]) <-
-      #        values(terra::rast(data$infected[[q]],
-      #                           crs = crs(infected_out), extent = ext(infected_out)))
-      #     terra::values(infected_out[[q]]) <- data$infected[[q]]
-      #     terra::values(susectible_out[[q]]) <- data$susceptible[[q]]
-      #     for (p in seq_len(length(data$exposed[[q]])))
-      #     terra::values(exposed_out[[q]]) <- data$exposed[[q]][[p]]
-      #   }
-      #
-      #   dir.create(paste(config$output_folder_path, "pops_runs/", sep = ""))
-      #   file_name <- paste(config$output_folder_path, "pops_runs/infected_", i, ".tif", sep = "")
-      #   terra::writeRaster(infected_out, file_name, overwrite = TRUE)
-      #   file_name <-
-      #   paste(config$output_folder_path, "pops_runs/susectible_", i, ".tif", sep = "")
-      #   terra::writeRaster(susectible_out, file_name, overwrite = TRUE)
-      #   file_name <- paste(config$output_folder_path, "pops_runs/exposed_", i, ".tif", sep = "")
-      #   terra::writeRaster(exposed_out, file_name, overwrite = TRUE)
-      # }
+      if (config$write_outputs == "all_simulations") {
+        infected_out <- terra::rast(config$infected_file)
+        susectible_out <- terra::rast(config$infected_file)
+        exposed_out <- terra::rast(config$infected_file)
+        for (q in seq_len(length(data$infected))) {
+          terra::values(infected_out[[q]]) <-
+            terra::values(terra::rast(data$infected[[q]],
+                                      crs = crs(infected_out), extent = ext(infected_out)))
+          terra::values(infected_out[[q]]) <- data$infected[[q]]
+          terra::values(susectible_out[[q]]) <- data$susceptible[[q]]
+          for (p in seq_len(length(data$exposed[[q]])))
+          terra::values(exposed_out[[q]]) <- data$exposed[[q]][[p]]
+        }
+
+        dir.create(paste(config$output_folder_path, "pops_runs/", sep = ""))
+        file_name <- paste(config$output_folder_path, "pops_runs/infected_", i, ".tif", sep = "")
+        terra::writeRaster(infected_out, file_name, overwrite = TRUE)
+        file_name <-
+        paste(config$output_folder_path, "pops_runs/susectible_", i, ".tif", sep = "")
+        terra::writeRaster(susectible_out, file_name, overwrite = TRUE)
+        file_name <- paste(config$output_folder_path, "pops_runs/exposed_", i, ".tif", sep = "")
+        terra::writeRaster(exposed_out, file_name, overwrite = TRUE)
+      }
 
       run
     }
