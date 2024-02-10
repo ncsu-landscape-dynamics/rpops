ffc1c560f13f3717bd854669883dbfd2f8e53250
Chris Jones <cjones1688@gmail.com>
2024-02-10 07:27:08 -0500

    add tests for cal and val

--- tests/testthat/test-validate.R
+++ tests/testthat/test-validate.R
@@ -828,3 +828,197 @@ test_that(
     expect_vector(data$infecteds_difference, size = number_of_iterations)
   }
 )
+
+test_that(
+  "Validation has correctly formatted returns/runs with polygon level data", {
+    skip_on_os("windows")
+    infected_years_file <-
+      system.file("extdata", "simple20x20", "county_infected_years.gpkg", package = "PoPS")
+    number_of_observations <- 68
+    parameter_means <- c(0, 16.4, 0.973, 7803, 0, 0, 0, 0)
+    parameter_cov_matrix <- matrix(0, nrow = 8, ncol = 8)
+    pest_host_table <-
+      system.file("extdata", "pest_host_table_singlehost_nomort.csv", package = "PoPS")
+    competency_table <- system.file("extdata", "competency_table_singlehost.csv", package = "PoPS")
+    checks <- c(500, 60000, 900, 1000)
+    infected_file_list <-
+      system.file("extdata", "simple20x20", "county_infected.gpkg", package = "PoPS")
+    host_file_list <-
+      system.file("extdata", "simple20x20", "host_w_sd2.tif", package = "PoPS")
+    total_populations_file <-
+      system.file("extdata", "simple20x20", "all_plants.tif", package = "PoPS")
+    temp <- FALSE
+    temperature_coefficient_file <- ""
+    precip <- FALSE
+    precipitation_coefficient_file <- ""
+    model_type <- "SI"
+    latency_period <- 0
+    time_step <- "month"
+    season_month_start <- 1
+    season_month_end <- 12
+    start_date <- "2003-01-01"
+    end_date <- "2003-12-31"
+    use_lethal_temperature <- FALSE
+    temperature_file <- ""
+    lethal_temperature <- -30
+    lethal_temperature_month <- 1
+    mortality_frequency <- "Year"
+    mortality_frequency_n <- 1
+    management <- FALSE
+    treatment_dates <- c("2003-01-24")
+    treatments_file <- ""
+    treatment_method <- "ratio"
+    natural_kernel_type <- "exponential"
+    anthropogenic_kernel_type <- "cauchy"
+    natural_dir <- "NONE"
+    natural_kappa <- 0
+    anthropogenic_dir <- "NONE"
+    anthropogenic_kappa <- 0
+    pesticide_duration <- c(0)
+    pesticide_efficacy <- 1.0
+    mask <- system.file("extdata", "simple20x20", "mask.tif", package = "PoPS")
+    output_frequency <- "year"
+    movements_file <- ""
+    use_movements <- FALSE
+    percent_natural_dispersal <- 1.0
+    anthropogenic_distance_scale <- 0.0
+    number_of_iterations <- 4
+    number_of_cores <- 2
+    start_exposed <- FALSE
+    generate_stochasticity <- TRUE
+    establishment_stochasticity <- TRUE
+    movement_stochasticity <- TRUE
+    dispersal_stochasticity <- FALSE
+    establishment_probability <- 0.5
+    dispersal_percentage <- 0.99
+    quarantine_areas_file <- ""
+    use_quarantine <- FALSE
+    output_frequency_n <- 1
+    use_spreadrates <- FALSE
+    use_overpopulation_movements <- FALSE
+    overpopulation_percentage <- 0
+    leaving_percentage <- 0
+    leaving_scale_coefficient <- 1
+    exposed_file_list <- ""
+    write_outputs <- "None"
+    output_folder_path <- ""
+    point_file <- ""
+    use_survival_rates <- FALSE
+    survival_rate_month <- 3
+    survival_rate_day <- 15
+    survival_rates_file <- ""
+    network_filename <- ""
+    network_movement <- "walk"
+    use_distance <- FALSE
+    use_configuration <- FALSE
+    use_initial_condition_uncertainty <- TRUE
+    use_host_uncertainty <- TRUE
+    weather_type <- "deterministic"
+    temperature_coefficient_sd_file <- ""
+    precipitation_coefficient_sd_file <- ""
+    dispersers_to_soils_percentage <- 0
+    quarantine_directions <- ""
+    multiple_random_seeds <- FALSE
+    file_random_seeds <- NULL
+    use_soils <- FALSE
+    soil_starting_pest_file <- ""
+    start_with_soil_populations <- FALSE
+    county_level_infection_data <- TRUE
+
+    outputs <- validate(
+      infected_years_file,
+      number_of_iterations,
+      number_of_cores,
+      parameter_means,
+      parameter_cov_matrix,
+      pest_host_table = pest_host_table,
+      competency_table = competency_table,
+      infected_file_list,
+      host_file_list,
+      total_populations_file,
+      temp,
+      temperature_coefficient_file,
+      precip,
+      precipitation_coefficient_file,
+      model_type,
+      latency_period,
+      time_step,
+      season_month_start,
+      season_month_end,
+      start_date,
+      end_date,
+      use_survival_rates,
+      survival_rate_month,
+      survival_rate_day,
+      survival_rates_file,
+      use_lethal_temperature,
+      temperature_file,
+      lethal_temperature,
+      lethal_temperature_month,
+      mortality_frequency,
+      mortality_frequency_n,
+      management,
+      treatment_dates,
+      treatments_file,
+      treatment_method,
+      natural_kernel_type,
+      anthropogenic_kernel_type,
+      natural_dir,
+      anthropogenic_dir,
+      pesticide_duration,
+      pesticide_efficacy,
+      mask,
+      output_frequency,
+      output_frequency_n,
+      movements_file,
+      use_movements,
+      start_exposed,
+      generate_stochasticity,
+      establishment_stochasticity,
+      movement_stochasticity,
+      dispersal_stochasticity,
+      establishment_probability,
+      dispersal_percentage,
+      quarantine_areas_file,
+      use_quarantine,
+      use_spreadrates,
+      use_overpopulation_movements,
+      overpopulation_percentage,
+      leaving_percentage,
+      leaving_scale_coefficient,
+      exposed_file_list,
+      write_outputs,
+      output_folder_path,
+      point_file,
+      network_filename,
+      network_movement,
+      use_distance,
+      use_configuration,
+      use_initial_condition_uncertainty,
+      use_host_uncertainty,
+      weather_type,
+      temperature_coefficient_sd_file,
+      precipitation_coefficient_sd_file,
+      dispersers_to_soils_percentage,
+      quarantine_directions,
+      multiple_random_seeds,
+      file_random_seeds,
+      use_soils,
+      soil_starting_pest_file,
+      start_with_soil_populations,
+      county_level_infection_data = county_level_infection_data)
+
+    expect_type(outputs, "list")
+    expect_length(outputs, 2)
+    data <- outputs[[1]]
+    expect_length(data, 24)
+    expect_vector(data$false_negatives, size = number_of_iterations)
+    expect_vector(data$false_positives, size = number_of_iterations)
+    expect_vector(data$true_positives, size = number_of_iterations)
+    expect_vector(data$true_negatives, size = number_of_iterations)
+    expect_vector(data$unknown_positives, size = number_of_iterations)
+    expect_vector(data$unknown_negatives, size = number_of_iterations)
+    expect_vector(data$odds_ratio, size = number_of_iterations)
+    expect_vector(data$residual_error, size = number_of_iterations)
+  }
+)
