60db727805ee47d08d9e9e25188cb93ec08e8279
cyborginhas <67606742+cyborginhas@users.noreply.github.com>
2024-11-19 08:12:24 -0500

    unique id appended to outputs (forecast_random_seeds.csv, simulated SEI matrices, etc.) from pops_lite.R

--- R/helpers.R
+++ R/helpers.R
@@ -361,3 +361,16 @@ update_config <- function(config) {
   
   return(config)
 }
+
+# Create unique ID to track forecast_random_seeds.csv and outputs from pops_lite.R
+# Function to generate a unique ID
+generate_unique_id <- function() {
+  # Generate interleaved sequence of random numbers and letters in one step
+  unique_id <- paste(c(rbind(
+    sample(1:9, 4, replace = TRUE), 
+    sample(letters, 4, replace = TRUE)
+  )), collapse = "")
+  
+  return(unique_id)
+}
+
