428a6d5129a73360c51239c6d4d7b5a735225001
Chris Jones <cjones1688@gmail.com>
2025-06-24 13:01:16 -0400

    lint

--- R/helpers.R
+++ R/helpers.R
@@ -267,7 +267,11 @@ create_random_seeds <- function(n) {
 # matrices must be the same size.
 matrix_norm_distribution <- function(mean_matrix, sd_matrix, total_population_matrix) {
   new_matrix <- matrix(round(truncnorm::rtruncnorm(
-    length(mean_matrix), a = 0, b = total_population_matrix[], mean = mean_matrix[], sd = sd_matrix[]
+    length(mean_matrix),
+    a = 0,
+    b = total_population_matrix[],
+    mean = mean_matrix[],
+    sd = sd_matrix[]
   )),
   nrow = nrow(mean_matrix),
   ncol = ncol(mean_matrix))
